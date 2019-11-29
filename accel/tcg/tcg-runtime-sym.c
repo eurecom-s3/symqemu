216b3e8cee43ac83daabf60c85c1c6f921df64e6
Sebastian Poeplau <sebastian.poeplau@eurecom.fr>
2019-11-29 15:20:21 +0100

    Instrument the "sub" instruction

--- accel/tcg/tcg-runtime-sym.c
+++ accel/tcg/tcg-runtime-sym.c
@@ -7,3 +7,17 @@ void HELPER(sym_add)(void *arg1, void *arg2)
 {
     qemu_printf("Now adding expressions %p and %p\n", arg1, arg2);
 }
+
+uint64_t HELPER(sym_sub_i64)(uint64_t arg1, uint64_t arg1_expr,
+                             uint64_t arg2, uint64_t arg2_expr)
+{
+    // TODO
+    return 0xAAAAAAAAAAAAAAAA;
+}
+
+uint64_t HELPER(sym_sub_i32)(uint32_t arg1, uint64_t arg1_expr,
+                             uint32_t arg2, uint64_t arg2_expr)
+{
+    // TODO
+    return 0xAAAAAAAAAAAAAAAA;
+}
--- accel/tcg/tcg-runtime-sym.h
+++ accel/tcg/tcg-runtime-sym.h
@@ -1,1 +1,3 @@
 DEF_HELPER_FLAGS_2(sym_add, TCG_CALL_NO_RWG, void, ptr, ptr)
+DEF_HELPER_FLAGS_4(sym_sub_i32, TCG_CALL_NO_RWG_SE, i64, i32, i64, i32, i64)
+DEF_HELPER_FLAGS_4(sym_sub_i64, TCG_CALL_NO_RWG_SE, i64, i64, i64, i64, i64)
