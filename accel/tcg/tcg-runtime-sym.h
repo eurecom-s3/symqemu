8ed17a26d98aea6d24ec6900245cb68cbf80e82c
Sebastian Poeplau <sebastian.poeplau@mailbox.org>
2021-01-16 17:52:22 +0100

    Add a README and license headers

--- accel/tcg/tcg-runtime-sym.h
+++ accel/tcg/tcg-runtime-sym.h
@@ -1,4 +1,20 @@
 /*
+ * This file is part of SymQEMU.
+ *
+ * SymQEMU is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * SymQEMU is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * SymQEMU. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
  * Symbolic helpers
  *
  * The helpers in this file perform symbolic computations corresponding to TCG's
