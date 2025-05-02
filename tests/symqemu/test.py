f44da94d76a7332b0457f2be3cc3972f72e40c0a
Romain Malmain <romain.malmain@pm.me>
2025-05-02 20:09:41 +0200

    compare unit tests using hashes

--- tests/symqemu/test.py
+++ tests/symqemu/test.py
@@ -1,10 +1,10 @@
-import filecmp
 import pathlib
 import shutil
 import unittest
 import tempfile
 import subprocess
 import os
+import hashlib
 
 import util
 
@@ -19,33 +19,20 @@ class SymQemuTests(unittest.TestCase):
 
         util.run_symqemu_on_test_binary(binary_name=binary_name, generated_test_cases_output_dir=symqemu_gen_output_dir)
 
-        # `filecmp.dircmp` does a "shallow" comparison, but this is not a problem here because
-        # the timestamps should always be different, so the actual content of the files will be compared.
-        # See https://docs.python.org/3/library/filecmp.html#filecmp.dircmp
-        expected_vs_actual_output_comparison = filecmp.dircmp(symqemu_gen_output_dir, symqemu_ref_output_dir)
+        expected_hashes = set()
+        for ref_file in symqemu_ref_output_dir.iterdir():
+            with open(ref_file, 'rb', buffering=0) as f:
+                expected_hashes.add(hashlib.file_digest(f, "sha256").hexdigest())
 
-        for diff_file in expected_vs_actual_output_comparison.diff_files:
-            ref_file = symqemu_ref_output_dir / diff_file
-            gen_file = symqemu_gen_output_dir / diff_file
+        testcase_not_found = False
+        for gen_file in symqemu_gen_output_dir.iterdir():
+            with open(gen_file, 'rb', buffering=0) as f:
+                f_hash = hashlib.file_digest(f, "sha256").hexdigest()
+                if not f_hash in expected_hashes:
+                    print(f"Error: content of file {gen_file} not found in expected testcases.");
+                    testcase_not_found = True
 
-            tmp_ref = tempfile.NamedTemporaryFile("w+")
-            subprocess.run(["xxd", f"{ref_file}"], stdout=tmp_ref, check=True)
-
-            tmp_gen = tempfile.NamedTemporaryFile("w+")
-            subprocess.run(["xxd", f"{gen_file}"], stdout=tmp_gen, check=True)
-
-            wdiff = subprocess.run(["wdiff", f"{tmp_ref.name}", f"{tmp_gen.name}"], capture_output=True)
-            colordiff = subprocess.run(["colordiff"], input=wdiff.stdout, capture_output=True)
-
-            print(f"===== Diff found in {diff_file} ======")
-            print(f"{colordiff.stdout.decode('utf-8').strip()}")
-            print(f"=================================")
-            print()
-
-        self.assertEqual(expected_vs_actual_output_comparison.diff_files, [])
-        self.assertEqual(expected_vs_actual_output_comparison.left_only, [])
-        self.assertEqual(expected_vs_actual_output_comparison.right_only, [])
-        self.assertEqual(expected_vs_actual_output_comparison.funny_files, [])
+        self.assertFalse(testcase_not_found)
 
     def test_simple(self):
         self.run_symqemu_and_assert_correct_result('simple')
